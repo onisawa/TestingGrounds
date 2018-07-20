b58b83b37b6f03b64baabcaafab4da03c583d5cb
Visava <onisawa@outlook.com>
2018-07-20 12:24:07 +0700

    Convert get value from blackboard in C++

--- Source/TestingGrounds/TestingGrounds.Build.cs
+++ Source/TestingGrounds/TestingGrounds.Build.cs
@@ -8,7 +8,7 @@ public class TestingGrounds : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks", "HeadMountedDisplay" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
