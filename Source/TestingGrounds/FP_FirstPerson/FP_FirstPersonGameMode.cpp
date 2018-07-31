26cf2a11c858145fa39ee99f3df3f732a3ac932b
Visava <onisawa@outlook.com>
2018-07-31 11:52:16 +0700

    restructure project

--- Source/TestingGrounds/FP_FirstPerson/FP_FirstPersonGameMode.cpp
+++ Source/TestingGrounds/FP_FirstPerson/FP_FirstPersonGameMode.cpp
@@ -2,13 +2,13 @@
 
 #include "FP_FirstPersonGameMode.h"
 #include "FP_FirstPersonHUD.h"
-#include "FP_FirstPersonCharacter.h"
+#include "Player/FirstPersonCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
 AFP_FirstPersonGameMode::AFP_FirstPersonGameMode()
 {
 	// Set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/Player"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Behaviour/Player"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// Use our custom HUD class
